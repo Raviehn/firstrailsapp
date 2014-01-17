fd2ec6b9645e4cec8de8ee4e18c7aa23b0a5f06d
j0k0 <jk_90@gmx.de>
2014-01-17 15:28:00 +0100

    db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Counter.create(team: 'BVB', goals: 1000000)
+Counter.create(team: 'Pauli', goals: 90)
+Counter.create(team: 'WOB', goals: 10)
+Counter.create(team: 'FCB', goals: -1)
+
