0c0ba128769ba50976b0ac6dd1871f468784bcc6
j0k0 <jk_90@gmx.de>
2014-01-30 16:23:31 +0100

    added landing page

---
+++ spec/requests/counters_landing_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe "Counters" do
+  describe "landing page" do
+    before(:each) do
+      create(:pauli)
+    end
+    it "shows the songs in the correct order" do
+        visit root_path
+        expect(page).to have_content 'Latest Team: FCSP'
+    end
+  end
+end
